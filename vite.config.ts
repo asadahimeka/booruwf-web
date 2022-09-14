d5a281ee92bc4434b9d7e61c61634f9117d48a62
asadahimeka <smallstar9637@yahoo.co.jp>
2022-09-15 01:22:12 +0800

    ✨ feat: support pwa

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { createVuePlugin as Vue2 } from 'vite-plugin-vue2'
 import { createHtmlPlugin as Html } from 'vite-plugin-html'
 import { viteExternalsPlugin as Externals } from 'vite-plugin-externals'
 import ScriptSetup from 'unplugin-vue2-script-setup/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -39,6 +40,85 @@ export default defineConfig({
     }),
     // https://github.com/antfu/unplugin-vue2-script-setup
     ScriptSetup(),
+    // https://github.com/antfu/vite-plugin-pwa
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'pwa-192x192.png', 'pwa-256x256.png'],
+      manifestFilename: 'manifest.json',
+      manifest: {
+        name: 'Booru Masonry',
+        short_name: 'booruwf',
+        description: 'Browsing booru sites with masonry layout.',
+        theme_color: '#B28FCE',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /.*\.css/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'css-cache',
+              cacheableResponse: {
+                statuses: [200],
+              },
+            },
+          },
+          {
+            urlPattern: /.*\.js/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'js-cache',
+              cacheableResponse: {
+                statuses: [200],
+              },
+            },
+          },
+          {
+            urlPattern: /.*\.(png|gif|jpg|jpeg|svg)/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'image-cache',
+              cacheableResponse: {
+                statuses: [200],
+              },
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 31536000,
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/(lib\.baomitu\.com|unpkg\.com|googleapis\.com|code\.bdstatic\.com)/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'cdn-cache',
+              cacheableResponse: {
+                statuses: [200],
+              },
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 31536000,
+              },
+              fetchOptions: {
+                credentials: 'include',
+              },
+            },
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
