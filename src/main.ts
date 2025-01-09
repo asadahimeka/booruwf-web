ddab6f418ddf0f4ab8e564db4d8e5091b0dc6dfa
Sakura Yumine <31837214+asadahimeka@users.noreply.github.com>
2025-01-09 10:09:23 +0800

    fix: add domain change notice

--- src/main.ts
+++ src/main.ts
@@ -15,3 +15,10 @@ const app = new Vue({
   render: h => h(App),
 })
 app.$mount('#app')
+
+if (location.host.includes('pixiv.pics')) {
+  const res = confirm('The current domain name is no longer in use. Would you like to visit another domain name instead?')
+  if (res) {
+    location.href = 'https://booru.cocomi.eu.org'
+  }
+}
